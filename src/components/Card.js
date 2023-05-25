9d1aca379381ee85c88e94de463f3a9c22723a4e
Ali Yousefi <alifordevelpment@gmail.com>
2023-05-25 22:31:01 +0430

    start project of 3d card

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <div className="w-[426px] min-h-[600px] bg-[#e4dfdc] "></div>
+    </div>
+  );
+};
+
+export default Card;
